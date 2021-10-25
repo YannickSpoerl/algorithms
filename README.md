08c1982d5919f3b44e87334d1fcbc86b037d2208
Yannick Spoerl <yannickspoerl@protonmail.com>
2021-10-25 14:15:04 +0200

    Implement edit-distance

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ A collection of common algorithms.
 |--------------|:-----:|-----------:|
 | *Dynamic Programming* |||
 | [knapsack.py](https://github.com/YannickSpoerl/algorithms/blob/main/dynamic_programming/knapsack.py) | [Knapsack problem](https://en.wikipedia.org/wiki/Knapsack_problem) | O(n ⋅ W) |
+| [edit_distance.py](https://github.com/YannickSpoerl/algorithms/blob/main/dynamic_programming/edit_distance.py) | [Edit distance](https://en.wikipedia.org/wiki/Edit_distance) | O(n ⋅ m) |
---
+++ dynamic_programming/edit_distance.py
@@ -0,0 +1,31 @@
+from typing import Callable
+
+
+def default_cost_func(char1: str, char2: str) -> int:
+    if char1 == char2:
+        return 0
+    return 1
+
+
+def edit_distance(string1: str, string2: str, delta_cost_func: Callable[[str, str], int] = default_cost_func):
+    """Given two string, and an optional cost-function for two chars, computes the minimum-distance"""
+    alignment_table = []
+    for i in range(0, len(string1) + 1):
+        alignment_table.append([])
+        for j in range(0, len(string2) + 1):
+            if i == 0 and j == 0:
+                alignment_table[i].append(0)
+            elif i == 0 and j != 0:
+                alignment_table[i].append(delta_cost_func("", string2[j - 1]))
+                continue
+            elif i != 0 and j == 0:
+                alignment_table[i].append(delta_cost_func(string1[i - 1], ""))
+                continue
+
+            cost_delete1 = alignment_table[i - 1][j] + delta_cost_func("", string2[j - 1])
+            cost_delete2 = alignment_table[i][j - 1] + delta_cost_func(string1[i - 1], "")
+            cost_change_chars = alignment_table[i - 1][j - 1] + delta_cost_func(string1[i - 1], string2[j - 1])
+
+            alignment_table[i].append(min(cost_delete1, cost_delete2, cost_change_chars))
+
+    return alignment_table[len(string1)][len(string2)]
